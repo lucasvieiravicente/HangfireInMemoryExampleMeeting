9ccf2bf232013d78b6c83b6e807ffb3049c61220
Lucas Vieira Vicente - LVI <lucas.vicente@iteris.com.br>
2021-04-12 19:39:58 -0300

    adicionado classe com exemplos

---
+++ Exemplos.cs
@@ -0,0 +1,48 @@
+﻿using Hangfire;
+using System;
+
+namespace ExemploInMemoryMeetingHangfire
+{
+    public class Exemplos
+    {
+        public Exemplos()
+        {
+            //Processamento "ative e esqueça"
+            string jobId = BackgroundJob.Enqueue(() => Console.WriteLine("Ative-e-esqueça!"));
+
+            //Processamento agendado
+            BackgroundJob.Schedule(() => Console.WriteLine("Agendado!"), TimeSpan.FromDays(7));
+            
+            //Processamento recorrente
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Recorrente!"), Cron.Daily);
+
+            //Processamento "continuado"
+            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine("Continuação de jobId!"));
+
+
+            /*
+             
+            -- Processos da versão PRO --
+
+
+            Processamento em lote:
+            
+            var batchId = BatchJob.StartNew(x =>
+            {
+                x.Enqueue(() => Console.WriteLine("Job 1"));
+                x.Enqueue(() => Console.WriteLine("Job 2"));
+            });
+
+            ------------------------------------------------------------------------
+
+            Processamento em lote "continuado":
+
+            BatchJob.ContinueBatchWith(batchId, x =>
+            {
+                x.Enqueue(() => Console.WriteLine("Last Job"));
+            });
+
+            */
+        }
+    }
+}
